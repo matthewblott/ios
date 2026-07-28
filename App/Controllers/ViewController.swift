e954cc20173672bb672b508db219cca203861c6e
Matthew Blott <m4ttb@pm.me>
2026-07-28 15:56:11 +0100

    Added more rules, mainly for modal pages.

--- App/Controllers/ViewController.swift
+++ App/Controllers/ViewController.swift
@@ -4,9 +4,8 @@ import UIKit
 class ViewController: HotwireWebViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-//    let properties = Hotwire.config.pathConfiguration.properties(for: currentVisitableURL)
-//    let hideBack = properties["hide_back_button"] as? Bool ?? false
-    let hideBack = true
+    let properties = Hotwire.config.pathConfiguration.properties(for: currentVisitableURL)
+    let hideBack = properties["hide_back_button"] as? Bool ?? true
     navigationItem.hidesBackButton = hideBack
   }
 }
