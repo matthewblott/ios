7c0055c318a78755e8aa99b4ea931b0a6d2360d8
Matthew Blott <m4ttb@pm.me>
2026-07-29 20:48:01 +0100

    Removed commented out code lines.

--- App/Delegates/SceneDelegate.swift
+++ App/Delegates/SceneDelegate.swift
@@ -5,7 +5,6 @@ let rootURL = URL(string: "http://localhost:5173/")!
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
-//  static var shared: SceneDelegate!
   private let navigator = Navigator(configuration: .init(
     name: "main",
     startLocation: rootURL
@@ -24,8 +23,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 extension SceneDelegate: NavigatorDelegate {
   func handle(proposal: VisitProposal) -> ProposalResult {
-//    print("🔍 URL: \(proposal.url)")
-//    print("🔍 properties: \(proposal.properties)")
     return .accept
   }
 }
